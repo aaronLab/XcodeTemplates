d2279bcd001172bbe7556a745372855e96f4aad2
Aaron Lee <aaronlab.net@gmail.com>
2021-12-28 12:43:29 +0900

    operation

---
+++ Operation.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___<T: ViewModelType>: AsyncOperation {
+  
+  private let viewModel: T
+  
+  var apiError: APIError?
+  var responseError: ResponseError?
+  var success: Bool = false
+  var onComplete: APIResponseProviderOnComplete?
+  
+  init(viewModel: T) {
+    self.viewModel = viewModel
+    
+    super.init()
+  }
+  
+  override func main() {
+    guard !isCancelled else { return }
+    
+    
+  }
+  
+}
+
+extension OperationTemplate: APIResponseProvider {}
